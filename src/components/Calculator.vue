a48ac4d9f984860afeca06ef492862ad6b0405cd
Lyndi Castrejon <lyndic@protonmail.com>
2020-02-09 21:57:51 -0600

    Add calculate functions to determine payoff time

---
+++ src/components/Calculator.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Debt Calculator</h1>
+    <h2>Find out how long it will take you to pay off debt!</h2>
+    <form v-on:submit.prevent="calculate(income, expenses, owed)">
+      <label>Monthly Income:</label>
+      <input v-model="income" type="number">
+      <label>Monthly Expenses:</label>
+      <input v-model="expenses" type="number">
+      <label>Total Debt Owed:</label>
+      <input v-model="owed" type="number">
+      <button type="submit">Calculate</button>
+    </form>
+    <div>{{result}}</div>
+    <button @click="clear" type="button">Start Over</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  data () {
+    return {
+      income: null,
+      expenses: null,
+      owed: null,
+      result: ''
+    }
+  },
+  methods: {
+    clear() {
+      this.result = '';
+    },
+    calculate(income, expenses, owed) {
+      let payment = income - expenses;
+      let payoffTime = owed / payment;
+      let conclusion = 'With the $' + payment + ' left over each month, it would take you ' +
+        payoffTime + ' months to pay off $' + owed;
+      this.result = `${conclusion}`;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
