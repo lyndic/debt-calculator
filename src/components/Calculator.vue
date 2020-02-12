1611d38afcaa056263d4027382f014986eaab877
Lyndi Castrejon <lyndic@protonmail.com>
2020-02-12 08:57:04 -0600

    Install bootstrap-vue and app

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,20 +1,63 @@
 <template>
-  <div>
-    <h1>Debt Calculator</h1>
-    <h2>Find out how long it will take you to pay off debt!</h2>
-    <form v-on:submit.prevent="calculate(income, expenses, owed, interest)">
-      <label>Monthly Income:</label>
-      <input v-model="income" type="number">
-      <label>Monthly Expenses:</label>
-      <input v-model="expenses" type="number">
-      <label>Total Debt Owed:</label>
-      <input v-model="owed" type="number">
-      <label>Annual Interest Rate:</label>
-      <input v-model="interest" type="number">
-      <button type="submit">Calculate</button>
-    </form>
-    <div>{{result}}</div>
-    <button @click="clear" type="button">Start Over</button>
+  <div class="container">
+    <b-jumbotron>
+      <h1>Debt Calculator</h1>
+      <h2>Find out how long it will take you to pay off debt!</h2>
+    </b-jumbotron>
+    <b-form v-on:submit.prevent="calculate(income, expenses, owed, interest)" @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Monthly Income:"
+        label-for="input-1"
+        description="Household income each month (in dollars)">
+        <b-form-input
+          id="input-1"
+          v-model="income"
+          type="float"
+          required
+          placeholder="2000"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-2"
+        label="Monthly Expenses:"
+        label-for="input-2"
+        description="Household expenses each month (in dollars)">
+        <b-form-input 
+          id="input-2"
+          v-model="expenses"
+          type="float"
+          required
+          placeholder="1400"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-3"
+        label="Total Debt Owed:"
+        label-for="input-3"
+        description="All debts owed (in dollars)">
+        <b-form-input
+          id="input-3"
+          v-model="owed"
+          type="float"
+          required
+          placeholder="10000"></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-4"
+        label="Annual Interest Rate:"
+        label-for="input-4"
+        description="Interest rate of debt (by percentage)">
+        <b-form-input
+          id="input-4"
+          v-model="interest"
+          type="float"
+          required
+          placeholder="5"></b-form-input>
+      </b-form-group>
+      <br>
+      <b-button type="submit" pill block variant="outline-success">Calculate</b-button>
+    </b-form>
+    <br>
+    <b-jumbotron class="result">{{result}}</b-jumbotron>
   </div>
 </template>
 
@@ -31,9 +74,6 @@ export default {
     }
   },
   methods: {
-    clear() {
-      this.result = '';
-    },
     calculate(income, expenses, owed, interest) {
       let payment = income - expenses;
       let adjustedDebt = parseFloat(owed) + parseFloat(owed * (interest / 100))
@@ -49,18 +89,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1 {
+    font-size: 50px;
+  }
+  h2 {
+    font-size: 25px;
+  }
+  .result {
+    text-align: center;
+  }
 </style>
