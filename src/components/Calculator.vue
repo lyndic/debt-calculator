ce8c0faeee7fc8a6cf86bf2e34ee5ff48fe8a50d
Lyndi Castrejon <lyndic@protonmail.com>
2020-02-13 08:36:48 -0600

    Add dynamic form field for debt owed and interest rate

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="container">
-    <b-jumbotron>
+  <div class="container card mt-4">
+    <b-jumbotron class="mt-4">
       <h1>Debt Calculator</h1>
       <h2>Find out how long it will take you to pay off debt!</h2>
     </b-jumbotron>
-    <b-form v-on:submit.prevent="calculate(income, expenses, owed, interest)" @submit="onSubmit">
+    <b-form v-on:submit.prevent="calculate(income, expenses, owed, interest)" 
+      @submit="onSubmit"
+      class="mb-4">
       <b-form-group
         id="input-group-1"
         label="Monthly Income:"
@@ -29,34 +31,39 @@
           required
           placeholder="1400"></b-form-input>
       </b-form-group>
-      <b-form-group
-        id="input-group-3"
-        label="Total Debt Owed:"
-        label-for="input-3"
-        description="All debts owed (in dollars)">
-        <b-form-input
-          id="input-3"
-          v-model="owed"
-          type="float"
-          required
-          placeholder="10000"></b-form-input>
-      </b-form-group>
-      <b-form-group
-        id="input-group-4"
-        label="Annual Interest Rate:"
-        label-for="input-4"
-        description="Interest rate of debt (by percentage)">
-        <b-form-input
-          id="input-4"
-          v-model="interest"
-          type="float"
-          required
-          placeholder="5"></b-form-input>
-      </b-form-group>
-      <br>
+        <form class="form-group" v-for="(debt, index) in debtForm" v-bind:key="index">
+          <input class="form-control mb-2" type="float" placeholder="Debt Owed" v-model="debt.owed">
+          <input class="form-control mb-2" type="float" placeholder="Annual Interest Rate" v-model="debt.interest">
+        </form>
+          <!-- <b-form-group
+            id="input-group-3"
+            label="Total Debt Owed:"
+            label-for="input-3"
+            description="All debts owed (in dollars)">
+            <b-form-input
+              id="input-3"
+              v-model="debt.owed"
+              type="float"
+              required
+              placeholder="10000"></b-form-input>
+          </b-form-group>
+          <b-form-group
+            id="input-group-4"
+            label="Annual Interest Rate:"
+            label-for="input-4"
+            description="Interest rate of debt (by percentage)">
+            <b-form-input
+              id="input-4"
+              v-model="debt.interest"
+              type="float"
+              required
+              placeholder="5"></b-form-input>
+          </b-form-group> -->
+        <b-button @click="addDebtField()" class="mb-4">
+          Add Debt
+        </b-button>
       <b-button type="submit" pill block variant="outline-success">Calculate</b-button>
     </b-form>
-    <br>
     <b-jumbotron class="result">{{result}}</b-jumbotron>
   </div>
 </template>
@@ -70,7 +77,11 @@ export default {
       expenses: null,
       owed: null,
       interest: null,
-      result: ''
+      result: '',
+      debtForm: [{
+        'owed': '',
+        'interest': '',
+      }],
     }
   },
   methods: {
@@ -82,6 +93,13 @@ export default {
       let conclusion = 'With the $' + payment + ' left over each month, it would take you ' +
         payoffTime + ' months to pay off $' + owed + ' at a ' + interest + '% rate.';
       this.result = `${conclusion}`;
+    },
+    addDebtField() {
+      this.debtForm.push({
+        'owed': '',
+        'interest': ''
+      }),
+      console.log('It has been pressed!')
     }
   }
 }
