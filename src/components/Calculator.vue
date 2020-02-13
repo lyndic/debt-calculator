70b8181e8380a7be0f4e67d3da347a4b6d3b32c3
Lyndi Castrejon <lyndic@protonmail.com>
2020-02-13 08:57:49 -0600

    Clean up form syntax and display number of debt h3 tags

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,66 +4,35 @@
       <h1>Debt Calculator</h1>
       <h2>Find out how long it will take you to pay off debt!</h2>
     </b-jumbotron>
-    <b-form v-on:submit.prevent="calculate(income, expenses, owed, interest)" 
-      @submit="onSubmit"
-      class="mb-4">
-      <b-form-group
-        id="input-group-1"
-        label="Monthly Income:"
-        label-for="input-1"
-        description="Household income each month (in dollars)">
-        <b-form-input
-          id="input-1"
+    <div class="mb-4" v-on:submit.prevent="calculate(income, expenses, owed, interest)" 
+      @submit="onSubmit">
+      <form class="form-group">
+        <input
+          class="form-control"
           v-model="income"
           type="float"
-          required
-          placeholder="2000"></b-form-input>
-      </b-form-group>
-      <b-form-group
-        id="input-group-2"
-        label="Monthly Expenses:"
-        label-for="input-2"
-        description="Household expenses each month (in dollars)">
-        <b-form-input 
+          placeholder="Monthly Income"
+          required/>
+      </form>
+      <form class="form-group">
+        <input 
+          class="form-control"
           id="input-2"
           v-model="expenses"
           type="float"
-          required
-          placeholder="1400"></b-form-input>
-      </b-form-group>
+          placeholder="Monthly Expenses"
+          required/>
+      </form>
         <form class="form-group" v-for="(debt, index) in debtForm" v-bind:key="index">
-          <input class="form-control mb-2" type="float" placeholder="Debt Owed" v-model="debt.owed">
-          <input class="form-control mb-2" type="float" placeholder="Annual Interest Rate" v-model="debt.interest">
+          <h3>Debt {{ index + 1 }}</h3>
+          <input class="form-control mb-3" type="float" placeholder="Debt Owed" v-model="debt.owed">
+          <input class="form-control" type="float" placeholder="Annual Interest Rate" v-model="debt.interest">
         </form>
-          <!-- <b-form-group
-            id="input-group-3"
-            label="Total Debt Owed:"
-            label-for="input-3"
-            description="All debts owed (in dollars)">
-            <b-form-input
-              id="input-3"
-              v-model="debt.owed"
-              type="float"
-              required
-              placeholder="10000"></b-form-input>
-          </b-form-group>
-          <b-form-group
-            id="input-group-4"
-            label="Annual Interest Rate:"
-            label-for="input-4"
-            description="Interest rate of debt (by percentage)">
-            <b-form-input
-              id="input-4"
-              v-model="debt.interest"
-              type="float"
-              required
-              placeholder="5"></b-form-input>
-          </b-form-group> -->
         <b-button @click="addDebtField()" class="mb-4">
           Add Debt
         </b-button>
       <b-button type="submit" pill block variant="outline-success">Calculate</b-button>
-    </b-form>
+    </div>
     <b-jumbotron class="result">{{result}}</b-jumbotron>
   </div>
 </template>
